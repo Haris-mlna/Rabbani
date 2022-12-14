3f0f2b640f471c691785a1d869268b75497a3414
Haris-mlna <harismlnaslm@gmail.com>
2022-12-14 17:40:17 +0700

    update pricing

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../scss/pricingpages.scss";
 
+import { RxAvatar } from "react-icons/rx";
+
 import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer.js";
 
@@ -10,8 +12,33 @@ function Pricing() {
       <Navbar />
       <div className="pricingpages">
         <div className="card-container">
-          <div className="card">Test 1</div>
-          <div className="card">Test 2</div>
+          <div className="card">
+            <h1>Card 1</h1>
+          </div>
+          <div className="card">
+            <h1>Card 2</h1>
+          </div>
+          <div className="card">
+            <h1>Card 3</h1>
+          </div>
+          <div className="card">
+            <h1>Card 4</h1>
+          </div>
+          <div className="card">
+            <h1>Card 5</h1>
+          </div>
+          <div className="card">
+            <h1>Card 6</h1>
+          </div>
+          <div className="card">
+            <h1>Card 7</h1>
+          </div>
+          <div className="card">
+            <h1>Card 8</h1>
+          </div>
+          <div className="card">
+            <h1>Card 9</h1>
+          </div>
         </div>
       </div>
       <Footer />
